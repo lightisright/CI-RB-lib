d3f3e99e99615d1d366656629de6634a57252ca7
lightisright <tournicotis-tournicotons@orange.fr>
2015-04-28 17:32:33 +0200

    change sample controller & view filenames

---
+++ controllers/Rbtest.php
@@ -0,0 +1,19 @@
+<?php
+
+class Rbtest extends CI_Controller {
+
+  public function __construct() {
+    parent::__construct();
+  }
+
+  public function index() {
+
+    $this->load->library('rb');
+    $news = R::findAll('news');
+
+    var_dump($news);
+
+    $data['news'] = $news;
+    $this->load->view('cirblibtest', $data);
+  }
+}
